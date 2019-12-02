de96837780125e1fd43a7960d16786d539046d71
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-02 10:07:36 -0600

    started

---
+++ DemoCarLoan/DemoCarLoan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DemoCarLoan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loan aLoan = new Loan();
+            aLoan.LoanNumber = 2239;
+            aLoan.LastName = "Mitchell";
+            aLoan.LoanAmount = 1000.00;
+            Console.WriteLine("Loan #{0} for {1} is for {2:C2}", aLoan.LoanNumber, aLoan.LastName, aLoan.LoanAmount);
+        }
+        class Loan
+        {
+            public int LoanNumber { get; set; }
+            public string LastName { get; set; }
+            public double LoanAmount { get; set; }
+        }
+    }
+}
