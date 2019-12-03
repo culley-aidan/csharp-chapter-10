83f8180de75ffdbe1938b8435b034a7d0e8bdcbf
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-03 09:03:25 -0600

    started

---
+++ PackageDemo/PackageDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
