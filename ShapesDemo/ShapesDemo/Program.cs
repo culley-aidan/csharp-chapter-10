18998ba35626122f7f9c2f01852423909e0509cf
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-05 08:50:27 -0600

    made Rectangle

--- ShapesDemo/ShapesDemo/Program.cs
+++ ShapesDemo/ShapesDemo/Program.cs
@@ -13,8 +13,23 @@ namespace ShapesDemo
         {
             public double Height { get; set; }
             public double Width { get; set; }
-            public double Area { get; }
+            public double Area { get => ComputeArea(); }
             public abstract double ComputeArea();
+            protected GeometricFigure(double height, double width)
+            {
+                Height = height;
+                Width = width;
+            }
+        }
+        class Rectangle : GeometricFigure
+        {
+            public Rectangle(double height, double width) : base(height, width)
+            {
+            }
+            public override double ComputeArea()
+            {
+                return Height * Width;
+            }
         }
     }
 }
