77b70159eb786881daa9762a281fa9a474069ed4
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-06 09:43:46 -0600

    Update Program.cs

--- SalespersonDemo/SalespersonDemo/Program.cs
+++ SalespersonDemo/SalespersonDemo/Program.cs
@@ -10,8 +10,42 @@ namespace SalespersonDemo
         }
         public interface ISellable
         {
-            void SalesSpeech();
-            void MakeSales();
+            string MakeSale();
+            string SalesSpeech();
+        }
+        public abstract class SalesPerson : ISellable
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string FullName { get => GetName(); }
+            public abstract string GetName();
+            public abstract string MakeSale();
+            public abstract string SalesSpeech();
+            protected SalesPerson(string first, string last)
+            {
+                FirstName = first;
+                LastName = last;
+            }
+        }
+        public class RealEstateSalesperson : SalesPerson
+        {
+            public RealEstateSalesperson(string first, string last) : base(first, last) { }
+            public override string GetName()
+                => $"{FirstName} {LastName}";
+            public override string SalesSpeech()
+                => "It's free real estate.";
+            public override string MakeSale()
+                => "Enter your name, date of birth, social security number";
+        }
+        public class GirlScout : SalesPerson
+        {
+            public GirlScout(string first, string last) : base(first, last) { }
+            public override string GetName()
+                => $"{FirstName} {LastName}";
+            public override string SalesSpeech()
+                => "BUY THE COOKIES.";
+            public override string MakeSale()
+                => "Enter your name and address here";
         }
 
     }
