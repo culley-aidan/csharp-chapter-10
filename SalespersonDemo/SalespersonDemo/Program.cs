3ad64c87831fda7ceee3aebc05584766c11074d3
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-06 09:18:33 -0600

    started

---
+++ SalespersonDemo/SalespersonDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SalespersonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World!");
+        }
+        public interface ISellable
+        {
+            void SalesSpeech();
+            void MakeSales();
+        }
+
+    }
+}
